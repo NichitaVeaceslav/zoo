740bb5f321a46018ee25e949704e3ab49cd9529f
Veaceslav Nichita <veaceslav.nichita@gmail.com>
2022-05-23 11:48:38 +0200

    AutoCompTV

--- app/src/main/java/com/gmail/veaceslav/nichita/monzoo/AlerteActivity.java
+++ app/src/main/java/com/gmail/veaceslav/nichita/monzoo/AlerteActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Checkable;
@@ -27,6 +29,11 @@ public class AlerteActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alerte);
 
+        AutoCompleteTextView tvLieux = findViewById(R.id.alerte_et_lieu);
+        String[] lieux = getResources().getStringArray(R.array.alerte_lieux);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,lieux);
+        tvLieux.setAdapter(aa);
     }
 
     public void envoi(View v) {
